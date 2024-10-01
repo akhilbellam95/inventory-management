704814e6dbdf95efe50dbb149a7b159f5430f0f2
Akhil Chowdary Bellam <akhilbellam95@gmail.com>
2024-10-01 17:53:41 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SidePanelComponent } from './components/side-panel/side-panel.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { SidePanelComponent } from "./components/side-panel/side-panel.component";
 import { HeaderComponent } from "./components/header/header.component";
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, SidePanelComponent, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'formonic-admin';
+  title = "inventory-management";
 
   isSidePanelOpen = false;
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
-import { Routes } from '@angular/router';
-import { ManageServicesComponent } from './screens/manage-services/manage-services.component';
-import { ManageVendorsComponent } from './screens/manage-vendors/manage-vendors.component';
-import { DashboardComponent } from './screens/dashboard/dashboard.component';
+import { Routes } from "@angular/router";
+import { ManageServicesComponent } from "./screens/manage-services/manage-services.component";
+import { ManageVendorsComponent } from "./screens/manage-vendors/manage-vendors.component";
+import { DashboardComponent } from "./screens/dashboard/dashboard.component";
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'manage-services', component: ManageServicesComponent },
-  { path: 'manage-vendors', component: ManageVendorsComponent },
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+  { path: "dashboard", component: DashboardComponent },
+  { path: "manage-services", component: ManageServicesComponent },
+  { path: "manage-vendors", component: ManageVendorsComponent },
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
 ];
